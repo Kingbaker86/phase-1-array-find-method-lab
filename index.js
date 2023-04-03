b219256b3122740755983910d0cd6442b10851d5
odaney baker <bakerobtb@gmail.com>
2023-04-02 20:55:55 -0400

    Done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+function superbowlWin(array) {
+  const win = array.find(obj => obj.result === "W");
+  if (win) {
+    return win.year;
+  } else {
+    return undefined;
+  }
+}
